0c30538c07696a77d552f6b25d36cd44612eadf6
Prithvipal Singh <prithvipal.singh@outlook.com>
2019-06-01 00:00:26 +0530

    added authentication with h2 database jdbc

--- src/main/java/com/spring/practice/security/SecurityConfig.java
+++ src/main/java/com/spring/practice/security/SecurityConfig.java
@@ -1,19 +1,28 @@
 package com.spring.practice.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		//auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		auth.jdbcAuthentication().dataSource(dataSource)
+		  .usersByUsernameQuery(
+		   "select username, password, 'true' as enabled from users where username=?")
+		  .authoritiesByUsernameQuery(
+		   "select username, role from user_roles where username=?");
 	}
 
 	@Override
@@ -22,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and().formLogin().loginPage("/login").permitAll();
 
 	}
+	
+	
 
 //	private CsrfTokenRepository csrfTokenRepository() {
 //		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
